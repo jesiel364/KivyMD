2684f3066e8233f7ecabded1ea6bde707e0174b6
jesiel364 <82860253+jesiel364@users.noreply.github.com>
2021-10-03 00:32:04 -0400

    Delete Changefy directory

--- Changefy/Mini.py
+++
@@ -1,8 +0,0 @@
-from kivymd.app import MDApp
-
-class Mini(MDApp):
-	def build(self):
-		self.theme_cls.theme_style = 'Dark'
-		self.theme_cls.primary_pallete = 'Dark_Blue'
-	
-Mini().run()
--- Changefy/mini_changefy.py
+++
@@ -1,9 +0,0 @@
-text = 'aaa'
-pattern = 'a'
-change_for = 'b'
-
-def replace_text():
-	res = text.replace(pattern, change_for)
-	print(res)
-	
-replace_text()
